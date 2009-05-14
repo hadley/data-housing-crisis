cf20ce2214baea212396827b4b18fc22ee859b45
hadley <h.wickham@gmail.com>
2009-05-14 17:27:15 -0500

    Download data for 2008

--- usps-vacancy/download.r
+++ usps-vacancy/download.r
@@ -1,7 +1,16 @@
-download.file(
-  "http://www.huduser.org/datasets/usps/downloads.odb?file=032009",
-  "temp.zip"
-)
-dbf <- unzip("temp.zip")
-file.rename(basename(dbf), "original/vacancy-2009-01.dbf")
-file.remove("temp.zip")
+get_dbf <- function(filenum, name) {
+  download.file(
+    paste("http://www.huduser.org/datasets/usps/downloads.odb?file=", filenum, sep = ""),
+    "temp.zip"
+  )
+  dbf <- unzip("temp.zip")
+  file.rename(basename(dbf), paste("original/", name, ".dbf", sep = ""))
+  file.remove("temp.zip")  
+}
+
+
+get_dbf("032009", "vacancy-2009-01")
+get_dbf("122008", "vacancy-2008-04")
+get_dbf("092008", "vacancy-2008-03")
+get_dbf("062008", "vacancy-2008-02")
+get_dbf("032008", "vacancy-2008-01")
