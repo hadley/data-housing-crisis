6e574a44d48a44ae3bbae21ed5d0e21393a5ca09
hadley <h.wickham@gmail.com>
2009-06-01 20:12:57 -0500

    Initial quick facts cleaning

--- census-quickfacts/clean.r
+++ census-quickfacts/clean.r
@@ -1,1 +1,19 @@
 # Source: http://quickfacts.census.gov/qfd/download_data.html
+options(stringsAsFactors = FALSE)
+
+qf <- read.csv("quickfacts.csv")
+
+# Give nicer abbreviations
+dd <- read.csv("data-dictionary.csv", sep = ";")
+names(qf) <- dd$abbr[match(names(qf), dd$field)]
+
+# Extract state and county fips codes
+codes <- formatC(qf$statecounty, width = 5, flag="0")
+qf$state <- as.numeric(substr(codes, 1, 2))
+qf$county <- as.numeric(substr(codes, 3, 5))
+qf$statecounty <- NULL
+
+# Remove state and overall aggregates
+qf <- subset(qf, county != 0)
+
+write.table(qf, "quickfacts-clean.csv", sep = ",", row = FALSE)
