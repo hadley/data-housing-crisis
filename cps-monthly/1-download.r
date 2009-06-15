1c5dc2321eb526b21412c55537c525ffd14b8ff2
hadley <h.wickham@gmail.com>
2009-06-15 18:00:20 -0500

    Tweaks to download script

--- cps-monthly/1-download.r
+++ cps-monthly/1-download.r
@@ -8,10 +8,17 @@ year2 <- as.numeric(substr(files, 4, 5))
 year <- ifelse(year2 < 20, 2000 + year2, 1900 + year2)
 
 df <- data.frame(month, year, url = urls, stringsAsFactors = FALSE)
-recent <- subset(df, year >= 2000)
-
 df$path <- with(df, 
   paste("raw/", year, "-", sprintf("%02i", month), ".txt.gz", sep = ""))
+recent <- subset(df, year >= 2000)
+
 
 library(plyr)
-m_ply(cbind(url = df$url, destfile = df$path), download.file)
+
+download_if_necessary <- function(path, url) {
+  if (!file.exists(path)) {
+    download.file(url, path)
+  }
+}
+
+m_ply(recent[c("path", "url")], download_if_necessary)
