f6e249e680f95dd81a1b910598db3c2405304893
DexReid <dexreid@gmail.com>
2009-06-17 15:25:40 -0500

    finished metro gdp and put up ahs data

--- gdp-metro/gdpmetro.R
+++ gdp-metro/gdpmetro.R
@@ -1,18 +1,12 @@
-# set up
+# source http://www.bea.gov/regional/gdpmetro/
+# set working directory to gdp-metro/raw
+setwd(choose.dir())
+rm(list=ls())
+source("clean_file.R")
 library(ggplot2)
 library(plyr)
-source(file.choose(), chdir=T)
-
-clean_file <- function(df) {
-df[df == "n/a" | df == "(D)" | df == "(L)"] <- NA
-df$Metropolitan.Area <- NULL
-df$Component <- NULL
-df$Industry <- NULL
-names(df) <- c("fips", "indcode", "comcode", "y2001", "y2002", 
-"y2003", "y2004", "y2005", "y2006")
-df <- subset(df, !(df$fips %in% c("Source: Bureau of Economic Analysis")))
-df <- subset(df, !(df$fips %in% c("FIPS")))}
-
+options(stringsAsFactors = FALSE)
 path <- dir(pattern = "\\.csv") 
 all_data <- llply(path, read.csv, stringsAsFactors = FALSE)
 clean_data <- ldply(all_data, clean_file)
+write.csv(clean_data, file = "clean_data.csv")
---
+++ gdp-metro/readme.md
@@ -0,0 +1,17 @@
+Metropolitan GDP
+================
+
+Cleaned data set has information regarding large metropolitan areas. The data is broken down into industry subsets such as agriculture or manufacturing. The fips codes for this data set are not the standard state-county type. They specifically refer to only the large metropolitan area they were assigned. Also, each industrial subset has been assigned a number in the data. There are dictionaries for both. Also, there are many missing values in this data, especially for smaller metro areas and smaller industries.
+
+Variables:
+
+*Year (2001-2006)
+*Industry
+*FIPS
+*GDP (in millions of current dollars)
+
+Data source:
+
+[General Website](http://www.bea.gov/regional/gdpmetro/)
+
+Link for data in zip file at bottom of the page.
