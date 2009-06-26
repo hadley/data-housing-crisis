8182f6eeeec1b0fdb6b192670ff1e39cd145b285
garrettgman <grolemund@gmail.com>
2009-06-26 16:05:13 -0500

    Cleaning up CPS monthly folder

--- cps-monthly/2-extract.r
+++ cps-monthly/2-extract.r
@@ -8,36 +8,36 @@ parse_line <- function(line, fields) {
   substring(line, fields$start, fields$start + fields$length - 1)  
 }
 
-parse_year <- function(year, month) {
-  path <- paste("raw/", year, "-", sprintf("%02i", month), ".txt.gz", sep = "")
-  out_path <- paste("clean/", year, "-", sprintf("%02i", month), ".csv.gz", sep = "")
-  
-  if (!file.exists(path)) return() # Skip if file doesn't exist
-  if (file.exists(out_path)) return() # Skip if already processed
-  
-  message("Processing ", path)
-  lines <- readLines(gzfile(path))
-  closeAllConnections()
-  
-  parsed <- llply(lines, parse_line, fields = to_extract, .progress = "text")
+parse_month <- function(year, month) {
+	path <- paste("raw/", year, "-", sprintf("%02i", month), ".txt.gz", sep = "")
+	out_path <- paste("clean/", year, "-", sprintf("%02i", month), ".csv.gz", sep = "")
+	
+	message("Processing ", path)
+	lines <- readLines(gzfile(path))
+	closeAllConnections()
 
-  # Convert to data frame
-  out <- do.call("rbind", parsed)
-  outdf <- as.data.frame(alply(out, 2, type.convert))
-  names(outdf) <- tolower(to_extract$abbr)
-  outdf$year <- year
-  outdf$month <- month
+	to_find <- to_extract[to_extract$year == year & to_extract$month == month,]
+	
+	parsed <- llply(lines, parse_line, fields = to_find, .progress = "text")
 
-  # Save as compressed csv file
-  write.table(outdf, gzfile(out_path), sep = ",", row = F)
-  closeAllConnections()
+	  # Convert to data frame
+	out <- do.call("rbind", parsed)
+	outdf <- as.data.frame(alply(out, 2, type.convert))
+	names(outdf) <- tolower(to_find$abbr)
+	outdf$year <- year
+	outdf$month <- month
+	
+	# Save as compressed csv file
+	write.table(outdf, gzfile(out_path), sep = ",", row = F)
+	closeAllConnections()
 }
 
-library(plyr)
+	
+
+
 
 all_months <- expand.grid(
   year = 2000:2009,
   month = 1:12
-)
-dir.create("clean")
-m_ply(all_months, parse_year)
+)[-c(50, 60, 70, 80, 90, 100, 110, 120),]
+m_ply(all_months, parse_month)
