80b3878292f67eec918e5d88e48a28907d90c7cb
bigbear <schloerke@gmail.com>
2009-06-30 12:52:31 -0500

    Changed up some plots.  Awesome

--- house-price-index/3-exports.r
+++ house-price-index/3-exports.r
@@ -53,13 +53,13 @@ maximum_hpi <- merge(MaxHPI, HPI09[,1:3])
 
 
 
-maximum_hpi$change <- maximum_hpi$hpi - maximum_hpi$hpi_2009
+maximum_hpi$percent_change <- maximum_hpi$hpi / maximum_hpi$hpi_2009 * 100 - 100
 
 #findings: the housing bubble- cities whoes HPI hit a peak in 2006,changed the most by 2009
 
 #ggplot(data= maximum_hpi, aes(x=time, y=change, label= state, colour=state)) + geom_point() + geom_text(angle=45, hjust= 0, size= 5) + opts(legend.position="none")
 
-findmerced <- ggplot(data= maximum_hpi, aes(x=time, y=change, label= state, colour=state)) + geom_point(size = 2) + geom_text( hjust = 0, vjust = 0, size= 5) + opts(legend.position="none", title = "Peak HPI vs. Time")
+findmerced <- ggplot(data= maximum_hpi, aes(x=time, y=percent_change, label= substr(state, 1, 2), colour=state)) + geom_text( hjust = .5, vjust = .5, size= 5) + opts(legend.position="none", title = "Peak HPI vs. Time")+ ylim(c(0,140))
 
 
 
@@ -73,10 +73,18 @@ findmerced <- ggplot(data= maximum_hpi, aes(x=time, y=change, label= state, colo
 
 
 	merced <- subset(hpi, city %in% c("Merced"))
-	mercedgraph <- qplot(x= time, y= hpi, data= merced, geom= "line", main = "Merced, CA") + ylim(c(0,350)) + opts(legend.position = "none") 
+	mercedMax <- data.frame(x = 2006.25, y = 321.29, label = "321.29")
+	merced09 <- data.frame(x = 2009, y = 140.4, label = "140.4")
+	mercedgraph <- qplot(x= time, y= hpi, data= merced, geom= "line", main = "Merced, CA") + 
+	  ylim(c(0,350)) + 
+	  opts(legend.position = "none") + 
+	  geom_point(data = mercedMax, aes(x, y), size= 2) + 
+	  geom_text(data = mercedMax, aes(x, y+5), label = "321.29", vjust = 0) + 
+	  geom_point(data = merced09, aes(x, y), size= 2) + 
+	  geom_text(data = merced09, aes(x, y+5), label = "140.4", vjust = 0)
 
 CA <- subset(maximum_hpi, state %in% c("CA"))
-	CAgraph <- ggplot(data= CA, aes(x=time, y=change, label= city)) + geom_point(size = 2) + geom_text( hjust = 0, vjust = 0, size= 5) + opts(title = "State of California",legend.position="none") + xlim(c(2005.5,2008))
+	CAgraph <- ggplot(data= CA, aes(x=time, y=percent_change, label= city)) + geom_point(size = 2) + geom_text( hjust = 0, vjust = 0, size= 5) + opts(title = "State of California",legend.position="none") + xlim(c(2005,2009)) + ylim(c(0,140))
 	
 
 
@@ -85,25 +93,25 @@ if(TRUE)
   print("printing")
   dir.create("exports")
   
-  pdf("exports/My Pretty Plots.pdf", width = 10, height = 8)
-  	print(statesAll)
-  	print(riseAndFall)
-  	print(bigRise)
-  dev.off()
+#  pdf("exports/My Pretty Plots.pdf", width = 8, height = 6)
+#  	print(statesAll)
+#  	print(riseAndFall)
+#  	print(bigRise)
+#  dev.off()
 
-  pdf("exports/Hpi Peak Time per State.pdf", width = 10, height = 8)
+  pdf("exports/Hpi Peak Time per State.pdf", width = 8, height = 6)
 	 print(findmerced)
 	dev.off()  
   
-  pdf("exports/Merced.pdf", width = 10, height = 8)
+  pdf("exports/Merced.pdf", width = 8, height = 6)
 	 print(mercedgraph)
 	dev.off()	
 	
-  pdf("exports/California.pdf", width = 10, height = 8)
+  pdf("exports/California.pdf", width = 8, height = 6)
 	 print(CAgraph)
 	dev.off()	 
 	
-	pdf("exports/Messy Plot.pdf", width = 10, height = 8)
+	pdf("exports/Messy Plot.pdf", width = 8, height = 6)
 	 print(qplot(time, hpi, data = hpi, colour = city, label = city_state,  geom = c("path","point","text"), main = "HPI vs. Time") + opts(legend.position = "none"))
 	dev.off()
 
