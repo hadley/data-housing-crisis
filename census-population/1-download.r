b980390e43bd1b7239c232e167848b899d44d16e
bigbear <schloerke@gmail.com>
2009-07-08 12:03:39 -0500

    Cleaned a metro census population data set for Gabi.  It prolly needs more work.

--- census-population/1-download.r
+++ census-population/1-download.r
@@ -4,3 +4,10 @@
 
 url <- "http://www.census.gov/popest/counties/files/CO-EST2008-ALLDATA.csv"
 download.file(url, "CO-EST2008-ALLDATA.csv")
+
+
+# Source: http://www.census.gov/popest/datasets.html
+#http://www.census.gov/popest/metro/files/2008/CBSA-EST2008-alldata.csv
+
+url <- "http://www.census.gov/popest/metro/files/2008/CBSA-EST2008-alldata.csv"
+download.file(url, "CBSA-EST2008-ALLDATA.csv")
--- census-population/2-clean.r
+++ census-population/2-clean.r
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 pop <- read.csv("CO-EST2008-ALLDATA.csv", stringsAsFactors = FALSE)
 names(pop) <- tolower(names(pop))
 pop <- rename(pop, c("gqestimatesbase2000" = "gqestimates2000"))
@@ -21,3 +23,36 @@ popm <- subset(popm, year != 2000)
 # Cast into desired output and save as csv
 clean <- cast(popm, state + county + year ~ variable)
 write.table(clean, "census-population.csv", sep = ",", row = FALSE)
+
+
+########################################################################
+########################################################################
+########################################################################
+
+
+met <- read.csv("CBSA-EST2008-ALLDATA.csv", skip = 4, stringsAsFactors = FALSE)
+names(met) <- tolower(strsplit(readLines("CBSA-EST2008-ALLDATA.csv")[1], ",")[[1]])
+
+# Remove non-yearly variables 
+met$census2000pop <- NULL
+met$estimatesbase2000 <- NULL
+
+
+# Melt data and separate variable name from year
+library(reshape)
+metm <- melt(met, id = 1:5)
+
+nc <- nchar(as.character(metm$variable))
+metm$year <- as.numeric(substr(metm$variable, nc - 3, nc))
+metm$variable <- substr(metm$variable, 1, nc - 4)
+
+# Remove 2001 because numbers are only for April-July
+metm <- subset(metm, year != 2000)
+
+# Cast into desired output and save as csv
+cleanMet <- cast(metm, year + cbsa + mdiv + stcou + name + lsad ~ variable)
+write.table(cleanMet, "census-population-by-metro.csv", sep = ",", row = FALSE)
+
+
+
+
