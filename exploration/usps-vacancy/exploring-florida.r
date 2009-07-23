03ef184b846fda673ab087a4b7a00ea58a6e2fa9
garrettgman <grolemund@gmail.com>
2009-07-23 16:23:56 -0500

    Wrote read me for usps vacancy folder. fixed links in code

--- exploration/usps-vacancy/exploring-florida.r
+++ exploration/usps-vacancy/exploring-florida.r
@@ -1,13 +1,33 @@
 # exploring vacancy data
+library(plyr)
 library(ggplot2)
-quartz()
-options(na.action = "na.exclude")
+library(R.oo)
+options(stringsAsFactors = F)
+savePlot <- function(..., plot= TRUE, big = FALSE)
+{
+nameOfPlot <- substitute(...)
+nameOfPlot <- gsub("_", "-", nameOfPlot)
+cat("\nPrinting plot ", nameOfPlot,".pdf in folder 'exports'", sep ="")
+if(plot)
+if(big)
+ggsave(..., file=paste("exports/", nameOfPlot,"-BIG.pdf",sep = "", collapse = ""), width=20, height=15) 
+else
+ggsave(..., file=paste("exports/", nameOfPlot,".pdf",sep = "", collapse = ""), width=8, height=6)
+else
+cat("\nJust Kidding!!!\n")
+cat("\n")
+}
 
-all <- read.csv(gzfile("new-vacancy.csv.gz"))
+all <- read.csv(gzfile("../../data/usps-vacancy/usps-vacancy.csv.gz"))
 
 # states with big bubbles have larger amounts of vacancies
-qplot(year + quarter/4, vac + nostat, data = all, facets = ~ statefips, main = "Number of vacant addresses")
-ggsave("all-states.pdf")
+vacancy_by_state <- qplot(year + quarter/4, vac + nostat, data = all, facets = ~ statefips, main = "Number of vacant addresses")
+vacancy_by_state
+savePlot(vacancy_by_state, big = T)
+
+per_vacancy_by_state <- qplot(year + quarter/4, (vac + nostat)/total, data = all, facets = ~ statefips, main = "Percent of addresses vacant")
+per_vacancy_by_state
+savePlot(per_vacancy_by_state, big = T)
 
 florida <- all[all$statefips == 12,]
 index <- function(x) x / x[1]
@@ -17,27 +37,41 @@ florida_all <- ddply(florida_all, .(countyfips), transform, nostat_ind = index(n
 
 # vacancies by county (Broward/Ft. Lauderdale, Palm Beach, and Miami-Dade lead the way)
 #_______________________________________________________________
-qplot(year + quarter/4, vac, data = florida, geom = "line", colour=countyfips, main = "Vacancies in Florida Counties")
+qplot(year + quarter/4, vac, data = florida, geom = "line", group = countyfips, colour=countyfips, main = "Vacancies in Florida Counties")
 # Broward County (Ft. Lauderdale leads the pack)
 
 # when indexed Flagler county stands out
 #________________________________________
-qplot(year + quarter/4, vac_ind, data = florida_all, geom = "line", colour=countyfips, main = "Vacancies in Florida Counties")
+FL_indexed <- qplot(year + quarter/4, vac_ind, data = florida_all, geom = "line", group = countyfips, colour=countyfips, main = "Vacancies in Florida Counties")
+FL_indexed
+savePlot(FL_indexed)
+
+florida_all$tot_vac <- with(florida_all, vac + nostat)
+florida_all <- ddply(florida, .(countyfips), transform, tot_ind = index(total))
 
 
+FL_tot_indexed <- qplot(year + quarter/4, tot_ind, data = florida_all, geom = "line", group = countyfips, colour=countyfips, main = "Total addresses in Florida Counties")
+FL_tot_indexed
+
+florida_all <- ddply(florida, .(countyfips), transform, tot_vac_ind = index(tot_vac))
+
+FL_total_vac_indexed <- qplot(year + quarter/4, tot_vac_ind, data = florida_all, geom = "line", group = countyfips, colour=countyfips, main = "Vacancies and nostats in Florida Counties")
+
+qplot(year + quarter/4, tot_vac, data = florida_all, geom = "line", group = countyfips, colour=countyfips, main = "Vacancies and nostats in Florida Counties")
 
 # of vacancy days sharply increases
 # _____________________________________
 quartz()
-qplot(year + quarter/4, vac_days, data = florida, geom = "line", colour=countyfips, main = "Total addresses in Florida Counties")
+qplot(year + quarter/4, vac_days, data = florida, geom = "line", group = countyfips, colour=countyfips, main = "Number of vacancy days in Florida Counties")
+
 
 
 # nostat unindexed
-qplot(year + quarter/4, nostat, data = florida_all, geom = "line", colour=countyfips, main = "Vacancies in Florida Counties")
+qplot(year + quarter/4, nostat, data = florida_all, geom = "line", group = countyfips, colour=countyfips, main = "Vacancies in Florida Counties")
 
 # nostat indexed all over the place
 #_____________________________
-qplot(year + quarter/4, nostat_ind, data = florida_all, geom = "line", colour=countyfips, main = "Vacancies in Florida Counties")
+qplot(year + quarter/4, nostat_ind, data = florida_all, geom = "line", group = countyfips, colour=countyfips, main = "Vacancies in Florida Counties")
 
 
 
---
+++ exploration/usps-vacancy/readme.md
@@ -0,0 +1,23 @@
+Vacancy Exploration
+===================
+by Garrett Grolemund
+
+I did an initial exploration of the USPS vacancy data.
+
+Observations
+================
+* Vacancy data is missing for the first quarter of 2008
+
+* Florida, California, and Texas appear to be the states that have the most counties with an above average of vacancies. Maricopa County, AZ (which includes the Phoenix valley area) has the highest number of vacant homes of any county in the US. It has about 45000 more vacant homes than the next closest county. These states do not stand out when you look at the percent of vacancies.
+
+* Within Florida: Broward, Palm Beach and Dade counties had the highest number of vacancies. (These countys correspond to Ft. Lauderdale, West Palm Beach, and Miami). When indexed, Flagler county (Palm Coast) had the steepest increase in vacancies. It was significantly higher than the rest of FL.
+
+* Most plots involving vacancies in Florida produced "spaghetti graphs" where no patterns or outliers could be spotted
+
+Conclusion
+===========
+Vacancies do not appear to be a complete measure of how badly a community was affected by the housing crisis.
+
+To do
+======
+Match vacancy data against HPI and other outcomes of the housing bubble.
