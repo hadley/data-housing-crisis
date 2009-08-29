f9f0c3bd7b69d28af67ffc42e649265a390fbe16
hadley <h.wickham@gmail.com>
2009-08-29 14:06:37 -0500

    Add 2008 data.  Tidy up parsing code

--- data/msa-changes/1-download.r
+++ data/msa-changes/1-download.r
@@ -5,8 +5,7 @@
 #http://www.census.gov/population/www/metroareas/lists/2006/List1.txt
 #http://www.census.gov/population/www/metroareas/lists/2007/List1.txt
 
-fileNames <- c("2003/03mfips.txt", "2003-12/0312mfips.txt", "2004/List1.txt", "2005/List1.txt", "2006/List1.txt", "2007/List1.txt", "historical/99mfips.txt")
-
+fileNames <- c("2003/03mfips.txt", "2003-12/0312mfips.txt", "2004/List1.txt", "2005/List1.txt", "2006/List1.txt", "2007/List1.txt","2008/List1.txt",  "historical/99mfips.txt")
 
 dir.create("original/")
 
@@ -18,7 +17,6 @@ get_txt <- function(name) {
   if (file.exists(to)) return()
   download.file(from, to)
 }
-
 
 for( i in fileNames)
   get_txt(i)
--- data/msa-changes/2-clean.r
+++ data/msa-changes/2-clean.r
@@ -3,133 +3,26 @@ library(R.oo)
 options(stringsAsFactors = FALSE)
 
 
-get_file <- function(fileName) 
-{
+load_file <- function(path) {
+  lines <- readLines(path, encoding = "Latin 1")
+  n <- length(lines)
   
-  linePos <- 1
-  rows <- readLines(fileName)
+  last <- which(lines[-n] == "" & lines[-1] == "")
 
-  while(TRUE)
-  {
-    
-    numbs <- as.character(1:10)
-#    print(numbs)
+  data <- lines[grepl("Statistical Area", lines)]
+  data <- gsub(" (Metropolitan|Micropolitan) Statistical Area", "", data)
+  data <- gsub(",", "", data)
+  
+  pieces <- strsplit(data, " {2,}")
+  pieces <- pieces[sapply(pieces, length) == 2]
+  
+  df <- as.data.frame(do.call("rbind", pieces))
+  names(df) <- c("msa_code", "city")
 
-#print(strsplit(row,"")[[1]][1])    
-    if(strsplit(rows[linePos],"")[[1]][1] %in% numbs)
-      break()
-    
-    linePos <- linePos + 1
-#    print(linePos)
-  }
-  
-  lineStart <- linePos
-  cat("\nLine Start");print(lineStart)
-  
-  while(TRUE)
-  {
-    
-    numbs <- as.character(1:10)
-#    print(numbs)
-
-#print(strsplit(row,"")[[1]][1])    
-    if(! strsplit(rows[linePos],"")[[1]][1] %in% c(numbs, " ", NA))
-      break()
-    
-    linePos <- linePos + 1
-#    print(linePos)
-  }
-  
-  lineEnd <- linePos-1
-  
-  cat("\nLine End");print(lineEnd)
-  
-  file <- rows[lineStart:lineEnd]
-  
-#  colnames(file) <- c("cbsa", "bad", "name")
-  
-#  file[,-2]
-  
-  return(file)
+  df
 }
 
-clean_file <- function(fileName)
-{
-  file <- get_file(fileName)
-  
-  file <- file[file != ""]
-  
-  
-  goodRows <- rep(FALSE, length(file))
-  for(i in 1:length(file))
-    if(substr(file[i], 25, 25)[[1]] != " ")
-      goodRows[i] <- TRUE
-      
-  
-  file <- file[goodRows]
-  
-  msa_code <- substr(file,1,5)
-  
-  name <- c()
-  for(i in 1:length(file))
-    name[i] <- substr(file[i], 25, nchar(file[i], type = "width"))
-  
-  name <- gsub("Micropolitan Statistical Area", "", name)
-  name <- gsub("Metropolitan Statistical Area", "", name)
-  name <- trim(name)
-  
-  year <- c()
-  fileName2 <- fileName
-  fileName2 <- gsub("original/", "", fileName2)
-  fileName2 <- gsub(".txt", "", fileName2)
-
-  if(length(strsplit(fileName2, "-")[[1]]) > 1)
-    year <- rep(as.numeric(strsplit(fileName2, "-")[[1]][1]) + (as.numeric(strsplit(fileName2, "-")[[1]][2]) - .5)/12, length(file))
-  else
-    year <- rep(as.numeric(fileName2), length(file))
-
-  Final <- data.frame(msa_code = msa_code, name = name, year = year)
-  
-  Final <- Final[!is.na(Final$name), ] # remove all weird names, 
-  #i.e. "10380                   Aguadilla-Isabela-San Sebasti\xe1n, PR Metropolitan Statistical Area"
-  
-  Final
-}
-
-
-a <- clean_file("original/2003.txt")
-b <- clean_file("original/2003-12.txt")
-
-d <- rbind(a,b)
-head(d[order(d[,1]), ])
-
-fileNames <- paste("original/", c("2003", "2003-12", as.character(2004:2007)), ".txt", sep = "")
-
-data <- NULL
-for(i in fileNames)
-  data <- rbind(data, clean_file(i))
-
-
-data$all_years <- rep(FALSE, nrow(data))
-for(i in unique(data$msa_code))
-{
-  tmp <- data[data$msa_code == i, ]
-  if(length(unique(tmp$year)) == length(fileNames))
-    data[data$msa_code == i, "all_years"] <- TRUE
-  if(length(unique(tmp$name)) == 1)
-    data[data$msa_code == i, "year"] <- -1
-}
-
-data <- unique(data)
-data <- data[order(data$msa_code), ]
-
-data$tmp <- paste(data$msa_code, data$name)
-data <- data[!duplicated(data$tmp), ]
-data$tmp <- NULL
-
-data <- data[,c("msa_code", "name")]
-
-
-write.csv(data, "msa-codes.csv", row.names= FALSE)
-
-
+files <- dir("original/", "^2", full.names = TRUE)
+data <- ldply(files, load_file)
+data <- unique(data[order(data$msa_code), ])
+write.table(data, "msa-codes.csv", row = FALSE, sep = ",")
