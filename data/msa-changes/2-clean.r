039707ca8f9913049ab493db558fc1030421ff53
hadley <h.wickham@gmail.com>
2009-08-29 16:08:24 -0500

    Create table of most recent MSA data suitable for labelling plots

--- data/msa-changes/2-clean.r
+++ data/msa-changes/2-clean.r
@@ -24,3 +24,22 @@ files <- dir("original/", "^2", full.names = TRUE)
 data <- ldply(files, load_file)
 data <- unique(data[order(data$msa_code), ])
 write.table(data, "msa-codes.csv", row = FALSE, sep = ",")
+
+# Output the most recent year, with major city extracted.  Use this for
+# labelling plots/tables etc.
+codes08 <- load_file("original/2008.txt")
+
+divider <- as.numeric(regexpr(" [A-Z-]+$", codes08$city))
+city <- substr(codes08$city, 1, divider - 1)
+state <- substr(codes08$city, divider + 1, 100)
+
+major_city <- sapply(strsplit(city, "-"), "[", 1)
+major_state <- sapply(strsplit(state, "-"), "[", 1)
+
+major <- data.frame(
+  msa_code = codes08$msa_code,
+  city = major_city,
+  state = major_state,
+  label = paste(abbreviate(major_city, 6), major_state, sep = "")
+)
+write.table(major, "msa-major.csv", row = FALSE, sep = ",")
