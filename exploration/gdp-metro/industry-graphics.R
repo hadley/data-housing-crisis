a7053c3a53a52bb67e8da487751d0f44cebee708
hadley <h.wickham@gmail.com>
2010-02-01 14:40:49 -0600

    Updates to gdp-metro exploration

--- exploration/gdp-metro/industry-graphics.R
+++ exploration/gdp-metro/industry-graphics.R
@@ -1,14 +1,49 @@
+library(stringr)
 library(ggplot2)
 
 gdp <- read.csv("../../data/gdp-metro/gdp-metro.csv")
 
-topcities <- subset(gdp, fips %in% c("35620", "31100","26420", "37980","38060", "41700", "41740", "19100", "41940",  "19820",  "26900",  "27260", "41860", "18140",  "12420", "32820", "12580"))
+selected <- subset(gdp, fips %in% c("35620", "31100","26420", "37980","38060", "41700", "41740", "19100", "41940",  "19820",  "26900",  "27260", "41860", "18140",  "12420", "32820", "12580") & indust %in% c("11", "3", "10", "12", "36", "45", "58", "63", "62", "66", "78", "67", "55", "12","71", "50", "6", "74", "100","104"))
+selected$X <- NULL
 
-industries_cities <- subset(topcities, indust %in% c("11", "3", "10", "12", "36", "45", "58", "63", "62", "66", "78", "67", "55", "12","71", "50", "6", "74", "100","104"))
+# Match with industry and city labels
 
-industries_cities <- industries_cities[!is.na(industries_cities$gdp),]
+indust <- read.csv("../../data/gdp-metro/indust-dictionary.csv")
+fips <-  read.csv("../../data/gdp-metro/fips-dictionary.csv")
 
-p <- qplot(x= year,y= gdp, data= industries_cities, colour = indust, group = indust, geom="line", facets=~ fips)
+selected <- merge(selected, indust, by = "indust")
+selected <- merge(selected, fips, by = "fips")
+
+# Make industry and city labels small enough to plot
+selected$industry <- abbreviate(selected$Industry, 8)
+selected$Industry <- NULL
+
+selected$metro <- str_replace(selected$Metropolitan.Area, " \\(MSA\\)", "")
+selected$metro <- abbreviate(selected$metro, 15)
+selected$Metropolitan.Area <- NULL
+
+selected <- selected[!is.na(selected$gdp), ]
+
+
+qplot(year, gdp, data = selected, colour = industry, geom="line", facets=~ metro, log = "y")x
+qplot(year, gdp, data = selected, colour = metro, geom="line", facets=~ industry, log = "y")
+
 # need to group by the industry as well as color it by industry.
 
-print(p)
+one <- subset(selected, fips == 12420 & indust == 36)
+lm(log(gdp) ~ I(year - 2001), data = one)
+
+growth <- ddply(selected, c("industry", "metro"), function(df) {
+  exp(coef(lm(log(gdp) ~ I(year - 2001), data = df)))
+})
+names(growth)[3:4] <- c("start", "growth")
+
+qplot(growth, industry, data = growth) + 
+  geom_vline(xintercept = 1, colour = "grey50")
+qplot(growth, metro, data = growth) + 
+  geom_vline(xintercept = 1, colour = "grey50")
+  
+qplot(start, growth, data = growth)
+
+# Next: remove suspiciously low starting values
+# 
