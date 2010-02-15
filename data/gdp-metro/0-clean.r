0ede642e39233d87fe3bf30b3243e7c54862176c
hadley <h.wickham@gmail.com>
2010-02-15 14:12:42 -0600

    Fix really stupid bug in data cleaning

--- data/gdp-metro/0-clean.r
+++ data/gdp-metro/0-clean.r
@@ -2,6 +2,9 @@ library(reshape)
 library(stringr)
 
 all <- read.csv("allgmp.csv")
+all <- subset(all, component_name == "GDP by Metropolitan Area (millions of current dollars)")
+all$component_name <- NULL
+
 all_m <- melt(all, m = paste("X", 2001:2008, sep = ""))
 
 # Convert variable to numeric year
@@ -9,7 +12,6 @@ all_m$year <- as.numeric(str_replace(all_m$variable, "X", ""))
 all_m$variable <- NULL
 
 # Focus only on gdp in millions of current dollars
-all_m <- subset(all_m, component_name == "GDP by Metropolitan Area (millions of current dollars)")
 
 gdp <- all_m[c("FIPS", "industry_id", "year", "value")]
 names(gdp) <- c("fips", "indust", "year", "gdp")
@@ -21,7 +23,7 @@ gdp <- gdp[!gdp$fips %in% c("FIPS", ""), ]
 gdp$code <- ifelse(gdp$gdp %in% c("n/a", "(D)", "(L)"), 
   as.character(gdp$gdp), NA)
 gdp$gdp[!is.na(gdp$code)] <- NA
-gdp$gdp <- as.numeric(gdp$gdp)
+gdp$gdp <- as.numeric(as.character(gdp$gdp))
 
 gdp$fips <- as.numeric(as.character(gdp$fips))
 gdp$indust <- as.numeric(as.character(gdp$indust))
