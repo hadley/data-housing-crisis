90ba14841defb702d3817c85dce326e3ef836256
gquart6 <gquart6@rice.edu>
2010-04-13 15:01:00 -0500

    further explorations

--- exploration/gdp-metro/yearly-gdp-hpi.R
+++ exploration/gdp-metro/yearly-gdp-hpi.R
@@ -1,3 +1,9 @@
+# describe overall patterns
+# TODO: colour CA, FL vs. others
+# describe individual city trajectories big.drop
+# figure out how to calculate mean correctly- if we want to calculate geometric mean it is (product(changes(i))^(1/n)
+## Isn't used in this code, but is used in others --> Annual growth rate = (Vpresent/Vpast)^(1/n)-1 NOT just total growthrate divided by number of years.
+
 library(ggplot2)
 library(stringr)
 library(plyr)
@@ -9,14 +15,89 @@ hpi <- read.csv("../../data/fhfa-house-price-index/fhfa-house-price-index-msa.cs
 #calculating yearly change: data is in quaters to took the yearly data from quater 4 only
 hpi.select<-subset(hpi,quarter==4) 
 
-#I got this code from gdp.delta which is the yearly change, but I am a little confused what diff does. The code works- I just don't know if it gives me what I want
+
 hpi.select <- ddply(hpi.select, c("fips_msa"), transform, 
   hpi.delta = c(diff(hpi) / diff(year), NA) / hpi,
   .progress = "text")
 
+hpi.gdp <- merge(gdp, hpi.select, 
+	by.x=c("fips", "year"), by.y= c("fips_msa", "year"))
 
-#??
-hpi.gdp <- merge(gdp, hpi.select, by.x="fips", by.y="fips_msa")
 
-#??
-qplot(hpi.delta, gdp.delta, data= subset(hpi.gdp, indust == 3)) + 	facet_wrap(~year)
+cons <- subset(hpi.gdp, indust == 11 & year < 2008)
+qplot(gdp.prop, gdp.delta, data = cons) + facet_wrap(~ year)
+qplot(gdp, gdp.delta, data = cons) + facet_wrap(~ year)
+
+# Pull out cities with > 50% change
+big.delta <- subset(cons, abs(gdp.delta) > 0.5)
+
+cons.safe <- subset(cons, !(fips %in% big.delta$fips))
+
+ggplot(cons.safe, aes(hpi.delta, gdp.delta)) +
+ geom_vline(xintercept = 0, colour = "grey50") + 
+ geom_hline(yintercept = 0, colour = "grey50") +
+ geom_point(aes(colour= state)) + 
+ facet_wrap(~ year, nrow = 2) + 
+ coord_equal()
+
+ggsave(..., file=paste("exports/", nameOfPlot,".pdf",sep = "", collapse = ""), width=8, height=6)
+
+# Look at changes in individual cities ----------------------------------
+# Is there a consistent pattern?
+
+# Compute per city summay statistics
+biggest <- ddply(cons.safe, c("metro", "fips"), summarise,
+	gdp.sd = sd(gdp.delta), 
+	hpi.sd = sd(hpi.delta),
+	gdp.mean = exp(mean(log(1 + gdp.delta))),
+	hpi.mean = exp(mean(log(1 + hpi.delta))),
+	n = length(hpi.delta))
+# Only want to look at cities with data for all years
+biggest <- subset(biggest, n == 7)
+qplot(gdp.mean, hpi.mean, data = biggest)
+qplot(gdp.sd, hpi.sd, data = biggest)
+
+# Make base plot
+base <- ggplot(mapping = aes(hpi.delta, gdp.delta)) + 
+  geom_vline(xintercept = 0, colour = "grey50") + 
+  geom_hline(yintercept = 0, colour = "grey50") +
+  geom_path(colour = "grey50") +
+  geom_point(aes(colour = year)) + 
+  facet_wrap(~ metro)
+  
+base %+% merge(cons.safe, subset(biggest, rank(hpi.mean) < 20))
+base %+% merge(cons.safe, subset(biggest, rank(-hpi.mean) < 20))
+
+# Pull out cities that had biggest drop in 2007
+big.drop <- subset(cons.safe, hpi.delta < -0.2 & year == 2007)$fips
+base %+% subset(cons.safe, fips %in% big.drop)
+#looking at this, initially delta.hpi and delta.gdp are even. There there is an steady and closely linear increase in both. Then there is a HUGE decrease in HPI, then a huge drop in GDP
+
+# Show average trajectory
+
+big.droppers <- subset(cons.safe, fips %in% big.drop)
+avg <- ddply(big.droppers, "year", summarise,
+	gdp.delta = mean(gdp.delta),
+	hpi.delta = mean(hpi.delta))
+
+ggplot(avg, aes(gdp.delta, hpi.delta)) +
+  geom_vline(xintercept = 0, colour = "grey50") + 
+  geom_hline(yintercept = 0, colour = "grey50") +
+  geom_path(colour = "grey50") +
+  geom_point(aes(colour = year)) + 
+  coord_equal()
+
+state_avgs <- ddply(cons.safe, c("state", "year"), summarise,
+	gdp.delta = mean(gdp.delta),
+	hpi.delta = mean(hpi.delta))
+state_avgs$state <- factor(state_avgs$state)
+state_avgs <- subset(state_avgs, str_length(state) == 2)
+
+ggplot(state_avgs, aes(gdp.delta, hpi.delta, group = state)) +
+  geom_vline(xintercept = 0, colour = "grey50") + 
+  geom_hline(yintercept = 0, colour = "grey50") +
+  geom_path(colour = "grey50") +
+  geom_point(aes(colour = year)) + 
+  facet_wrap(~ state)+ 
+  coord_equal()
+#it might be true that if house prices increased, but there was no major increase in GDP a city was safe
